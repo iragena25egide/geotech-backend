1da8f2a74afb3b47f6928ea6dd560dea357c45ca
iragena25egide <iragenaegide205@gmail.com>
2026-04-22 22:43:20 +0200

    soil component

---
+++ src/soil/soil.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { SoilService } from './soil.service';
+import { SoilController } from './soil.controller';
+
+@Module({
+  providers: [SoilService],
+  controllers: [SoilController]
+})
+export class SoilModule {}
