2ff1f81a58f4c772eb701deb1dba1f3d8ab3d29c
iragena25egide <iragenaegide205@gmail.com>
2026-04-26 20:35:14 +0200

    report component

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { ReportService } from './report.service';
+import { Report } from '../entities/report.entity';
 
-@Controller('report')
-export class ReportController {}
+@Controller('reports')
+export class ReportController {
+  constructor(private readonly reportService: ReportService) {}
+
+  @Post()
+  async create(@Body() data: Partial<Report>): Promise<Report> {
+    return this.reportService.create(data);
+  }
+
+  @Get()
+  async findAll(): Promise<Report[]> {
+    return this.reportService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Report | null> {
+    return this.reportService.findOne(Number(id));
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() data: Partial<Report>,
+  ): Promise<Report | null> {
+    return this.reportService.update(Number(id), data);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<void> {
+    return this.reportService.remove(Number(id));
+  }
+}
--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from '../entities/report.entity';
 
 @Injectable()
-export class ReportService {}
+export class ReportService {
+  constructor(
+    @InjectRepository(Report)
+    private readonly reportRepository: Repository<Report>,
+  ) {}
+
+  async create(data: Partial<Report>): Promise<Report> {
+    const report = this.reportRepository.create(data);
+    return this.reportRepository.save(report);
+  }
+
+  async findAll(): Promise<Report[]> {
+    return this.reportRepository.find();
+  }
+
+  async findOne(id: number): Promise<Report | null> {
+    return this.reportRepository.findOne({ where: { id } });
+  }
+
+  async update(id: number, data: Partial<Report>): Promise<Report | null> {
+    await this.reportRepository.update(id, data);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.reportRepository.delete(id);
+  }
+}
