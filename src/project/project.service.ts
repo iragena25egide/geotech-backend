6f8a98ceefcaa875e69d04115fa23b9f26ea0b13
iragena25egide <iragenaegide205@gmail.com>
2026-04-26 20:21:28 +0200

    project service

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Project } from '../entities/project.entity';
 
 @Injectable()
-export class ProjectService {}
+export class ProjectService {
+  constructor(
+    @InjectRepository(Project)
+    private readonly projectRepository: Repository<Project>,
+  ) {}
+
+  async create(data: Partial<Project>): Promise<Project> {
+    const project = this.projectRepository.create(data);
+    return this.projectRepository.save(project);
+  }
+
+  async findAll(): Promise<Project[]> {
+    return this.projectRepository.find();
+  }
+
+  async findOne(id: number): Promise<Project | null> {
+    return this.projectRepository.findOne({ where: { id } });
+  }
+
+  async update(id: number, data: Partial<Project>): Promise<Project | null> {
+    await this.projectRepository.update(id, data);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.projectRepository.delete(id);
+  }
+}
