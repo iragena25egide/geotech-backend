aec2c278fa9e1ec09b3deb6a97713a09ab7b5a24
iragena25egide <iragenaegide205@gmail.com>
2026-04-26 20:31:18 +0200

    soil component

--- src/soil/soil.controller.ts
+++ src/soil/soil.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { SoilService } from './soil.service';
+import { SoilSample } from '../entities/soil.entity';
 
-@Controller('soil')
-export class SoilController {}
+@Controller('soil-samples')
+export class SoilController {
+  constructor(private readonly soilService: SoilService) {}
+
+  @Post()
+  async create(@Body() data: Partial<SoilSample>): Promise<SoilSample> {
+    return this.soilService.create(data);
+  }
+
+  @Get()
+  async findAll(): Promise<SoilSample[]> {
+    return this.soilService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<SoilSample | null> {
+    return this.soilService.findOne(Number(id));
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() data: Partial<SoilSample>,
+  ): Promise<SoilSample | null> {
+    return this.soilService.update(Number(id), data);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<void> {
+    return this.soilService.remove(Number(id));
+  }
+}
--- src/soil/soil.service.ts
+++ src/soil/soil.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { SoilSample } from '../entities/soil.entity';
 
 @Injectable()
-export class SoilService {}
+export class SoilService {
+  constructor(
+    @InjectRepository(SoilSample)
+    private readonly soilRepository: Repository<SoilSample>,
+  ) {}
+
+  async create(data: Partial<SoilSample>): Promise<SoilSample> {
+    const soil = this.soilRepository.create(data);
+    return this.soilRepository.save(soil);
+  }
+
+  async findAll(): Promise<SoilSample[]> {
+    return this.soilRepository.find();
+  }
+
+  async findOne(id: number): Promise<SoilSample | null> {
+    return this.soilRepository.findOne({ where: { id } });
+  }
+
+  async update(
+    id: number,
+    data: Partial<SoilSample>,
+  ): Promise<SoilSample | null> {
+    await this.soilRepository.update(id, data);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.soilRepository.delete(id);
+  }
+}
