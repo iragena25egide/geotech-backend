1db08a3d5366a4f12e8db428be69548de020a4d9
iragena25egide <iragenaegide205@gmail.com>
2026-04-26 20:27:04 +0200

    project controller

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { ProjectService } from './project.service';
+import { Project } from '../entities/project.entity';
 
-@Controller('project')
-export class ProjectController {}
+@Controller('projects')
+export class ProjectController {
+  constructor(private readonly projectService: ProjectService) {}
+
+  @Post()
+  async create(@Body() data: Partial<Project>): Promise<Project> {
+    return this.projectService.create(data);
+  }
+
+  @Get()
+  async findAll(): Promise<Project[]> {
+    return this.projectService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Project | null> {
+    return this.projectService.findOne(Number(id));
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() data: Partial<Project>,
+  ): Promise<Project | null> {
+    return this.projectService.update(Number(id), data);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<void> {
+    return this.projectService.remove(Number(id));
+  }
+}
